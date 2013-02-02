4b5004c96732c07c286f1caec807c933db680b4e
Zoltan Debre <zdebre@gmail.com>
2013-02-02 18:34:01 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zitter::Application.config.secret_token = '245524063e86ff260ab4eeef36103d262e6e8561801a088ac388cc01cad39f5f1d564c0cca01ea2f821a4f4a19e1fbdbe6b7fda8e25cf92b4bdd4d23f94cc7b2'
